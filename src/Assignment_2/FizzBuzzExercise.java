0f53874b24bea8a2409d32449ee9abbf6a80adbd
chenrui-hu <chenrui_hu1994@163.com>
2018-03-03 23:35:52 +0800

    assginment_2 part 3, fuzzbuzz exercise.

--- src/Assignment_2/FizzBuzzExercise.java
+++ src/Assignment_2/FizzBuzzExercise.java
@@ -1,4 +1,30 @@
 package Assignment_2;
 
 public class FizzBuzzExercise {
+
+    public static void FizzBuzz(int num){
+        if(num%3 == 0 & num%5 == 0){
+            System.out.println("FizzBuzz");
+        }
+        else if(num%3 == 0){
+            System.out.println("Fizz");
+        }
+        else if(num%5 == 0){
+            System.out.println("Buzz");
+        }
+        else{
+            System.out.println(num);
+        }
+
+    }
+
+    public static void main(String[] args){
+
+        int n = 1;
+        for(n = 1; n < 20; n++){
+            FizzBuzz(n);
+        }
+
+    }
 }
+
